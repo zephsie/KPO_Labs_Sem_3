25a90b37083c3ca4b4a16e604eae57a18f43c8a3
zephsie <zephiezephs@gmail.com>
2022-10-16 15:20:00 +0300

    0/10

--- Task9.c
+++ Task9.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+typedef struct Node {
     int data;
     struct Node *next;
     struct Node *prev;
-};
+} Node;
 
-struct Node *head = NULL;
-struct Node *tail = NULL;
+Node *head = NULL;
+Node *tail = NULL;
 int size = 0;
+
+Node *createNode(int data) {
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
 
 void printList() {
     if (head == NULL) {
@@ -17,7 +25,7 @@ void printList() {
         return;
     }
 
-    struct Node *temp = head;
+    Node *temp = head;
     while (temp != NULL) {
         printf("[%d]", temp->data);
         temp = temp->next;
@@ -27,10 +35,7 @@ void printList() {
 }
 
 void insertAtBeginning(int data) {
-    struct Node *newNode = malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    newNode->prev = NULL;
+    Node *newNode = createNode(data);
 
     if (head == NULL) {
         head = newNode;
@@ -45,10 +50,7 @@ void insertAtBeginning(int data) {
 }
 
 void insertAtEnd(int data) {
-    struct Node *newNode = malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    newNode->prev = NULL;
+    Node *newNode = createNode(data);
 
     if (head == NULL) {
         head = newNode;
@@ -63,33 +65,45 @@ void insertAtEnd(int data) {
 }
 
 void insertAtPosition(int data, int position) {
-    if (position <= 1) {
+    if (position < 0) {
+        return;
+    }
+
+    if (position == 0) {
         insertAtBeginning(data);
         return;
     }
 
-    if (position > size) {
+    if (position >= size) {
         insertAtEnd(data);
         return;
     }
 
-    struct Node *newNode = malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    newNode->prev = NULL;
+    Node *newNode = createNode(data);
 
-    struct Node *temp = head;
-    int i = 1;
+    if (position < size / 2) {
+        Node *temp = head;
+        for (int i = 0; i < position - 1; i++) {
+            temp = temp->next;
+        }
 
-    while (i < position - 1) {
-        temp = temp->next;
-        i++;
+        newNode->next = temp->next;
+        temp->next->prev = newNode;
+        temp->next = newNode;
+        newNode->prev = temp;
+    } else {
+        Node *temp = tail;
+        for (int i = size - 1; i > position; i--) {
+            temp = temp->prev;
+        }
+
+        newNode->prev = temp->prev;
+        temp->prev->next = newNode;
+        temp->prev = newNode;
+        newNode->next = temp;
     }
 
-    newNode->next = temp->next;
-    temp->next->prev = newNode;
-    temp->next = newNode;
-    newNode->prev = temp;
+    size++;
 }
 
 void deleteAtBeginning() {
@@ -97,7 +111,7 @@ void deleteAtBeginning() {
         return;
     }
 
-    struct Node *temp = head;
+    Node *temp = head;
     if (head == tail) {
         head = NULL;
         tail = NULL;
@@ -115,7 +129,7 @@ void deleteAtEnd() {
         return;
     }
 
-    struct Node *temp = tail;
+    Node *temp = tail;
     if (head == tail) {
         head = NULL;
         tail = NULL;
@@ -129,28 +143,42 @@ void deleteAtEnd() {
 }
 
 void deleteAtPosition(int position) {
-    if (position <= 1) {
+    if (position < 0 || position >= size) {
+        return;
+    }
+
+    if (position == 0) {
         deleteAtBeginning();
         return;
     }
 
-    if (position >= size) {
+    if (position == size - 1) {
         deleteAtEnd();
         return;
     }
 
-    struct Node *temp = head;
-    int i = 1;
+    if (position < size / 2) {
+        Node *temp = head;
+        for (int i = 0; i < position - 1; i++) {
+            temp = temp->next;
+        }
 
-    while (i < position - 1) {
-        temp = temp->next;
-        i++;
+        Node *temp2 = temp->next;
+        temp->next = temp2->next;
+        temp2->next->prev = temp;
+        free(temp2);
+    } else {
+        Node *temp = tail;
+        for (int i = size - 1; i > position + 1; i--) {
+            temp = temp->prev;
+        }
+
+        Node *temp2 = temp->prev;
+        temp->prev = temp2->prev;
+        temp2->prev->next = temp;
+        free(temp2);
     }
 
-    struct Node *temp2 = temp->next;
-    temp->next = temp2->next;
-    temp2->next->prev = temp;
-    free(temp2);
     size--;
 }
 
@@ -196,131 +224,6 @@ void clean() {
     }
 }
 
-int readInt() {
-    int n;
-    scanf_s("%d", &n);
-    return n;
-}
-
 int main() {
-    for (int i = 0; i < 5; i++) {
-        printf("Inserting %d at beginning: ", i);
-        insertAtBeginning(i);
-        printList();
-    }
-
-    for (int i = 0; i < 5; i++) {
-        printf("Inserting %d at end: ", i + 5);
-        insertAtEnd(i + 5);
-        printList();
-    }
-
-    for (int i = 0; i < 5; i++) {
-        printf("Inserting %d at position %d: ", i + 10, i * 2);
-        insertAtPosition(i + 10, i * 2);
-        printList();
-    }
-
-    for (int i = 0; i < 5; i++) {
-        printf("Deleting at beginning: ");
-        deleteAtBeginning();
-        printList();
-    }
-
-    for (int i = 0; i < 5; i++) {
-        printf("Deleting at end: ");
-        deleteAtEnd();
-        printList();
-    }
-
-    printf("Deleting at position 3: ");
-    deleteAtPosition(3);
-    printList();
-
-    printf("Deleting at position 1: ");
-    deleteAtPosition(1);
-    printList();
-
-    printf("Deleting at position 3: ");
-    deleteAtPosition(3);
-    printList();
-
-    printf("Peeking first %d: ", peekFirst());
-    printList();
-
-    printf("Peeking last %d: ", peekLast());
-    printList();
-
-    printf("Popping first %d: ", popFirst());
-    printList();
-
-    printf("Popping last %d: ", popLast());
-    printList();
-
-    for (int i = 0; i < 5; i++) {
-        printf("Inserting %d at beginning: ", i);
-        insertAtBeginning(i);
-        printList();
-    }
-
-    printf("Cleaning list: ");
-    clean();
-    printList();
-
-    while (1) {
-        printf("1. Insert at beginning\n2. Insert at end\n3. Insert at position\n4. Delete at beginning\n5. Delete at end\n"
-               "6. Delete at position\n7. Peek first\n8. Peek last\n9. Pop first\n10. Pop last\n11. Print list\n12. Clean\n13. Exit\n");
-
-        printf("Enter your choice:");
-
-        switch (readInt()) {
-            case 1:
-                printf("Enter data:");
-                insertAtBeginning(readInt());
-                break;
-            case 2:
-                printf("Enter data:");
-                insertAtEnd(readInt());
-                break;
-            case 3:
-                printf("Enter data:");
-                int data = readInt();
-                printf("Enter position:");
-                int position = readInt();
-                insertAtPosition(data, position);
-                break;
-            case 4:
-                deleteAtBeginning();
-                break;
-            case 5:
-                deleteAtEnd();
-                break;
-            case 6:
-                printf("Enter position:");
-                deleteAtPosition(readInt());
-                break;
-            case 7:
-                printf("First element: %d", peekFirst());
-                break;
-            case 8:
-                printf("Last element: %d", peekLast());
-                break;
-            case 9:
-                printf("Popped element: %d", popFirst());
-                break;
-            case 10:
-                printf("Popped element: %d", popLast());
-                break;
-            case 11:
-                printList();
-                break;
-            case 12:
-                clean();
-                break;
-            case 13:
-                exit(0);
-            default:
-                printf("Invalid choice");
-        }
-    }
+    return 0;
 }
